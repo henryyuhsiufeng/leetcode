722ba24d397f112b0e3d01fb51bfb66b18212f11
Henry Feng <henryfeng12345@gmail.com>
2020-12-19 12:17:54 -0600

    add rotate array solution

---
+++ rotate-array-sol.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  //mod the k times with length of nums array
+  let rotIndex = k % nums.length;
+  //reverse the whole array
+  reverse(nums, 0, nums.length - 1);
+  //reverse the segmented array at rotIndex
+  console.log(rotIndex);
+  reverse(nums, 0, rotIndex - 1);
+  reverse(nums, rotIndex, nums.length - 1);
+  return nums;
+};
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+}
