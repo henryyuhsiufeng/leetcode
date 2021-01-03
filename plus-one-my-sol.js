6e925db71061e7da9a708c8ccb42ec96b35817e4
Henry Feng <henryfeng12345@gmail.com>
2021-01-03 11:35:09 -0600

    add plus one my solution

---
+++ plus-one-my-sol.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  // let index = digits.length-1
+  for (let index = digits.length - 1; index >= 0; index--) {
+    // case if index is 9
+    if (digits[index] === 9) {
+      digits[index] = 0;
+      // case if need to add new digit
+      if (index - 1 === -1) {
+        digits.unshift(1);
+        return digits;
+      }
+    } else {
+      // case normal addition
+      digits[index]++;
+      return digits;
+    }
+  }
+};
